1c4d095cb1769a64285549afb719c2938f9e7fe9
DavidTausend <davidtausend-911@hotmail.com>
2023-11-18 14:12:04 +0000

    fix footer fonts distribution with another js file

---
+++ assets/js/footer.js
@@ -0,0 +1,5 @@
+//Font Awesome kit
+var faScript = document.createElement('script');
+faScript.src = 'https://kit.fontawesome.com/ad080cc682.js';
+faScript.crossOrigin = 'anonymous';
+document.head.appendChild(faScript);

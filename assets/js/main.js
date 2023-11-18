83b27001908812b203c9c197ee00b1bb0455c0b9
DavidTausend <davidtausend-911@hotmail.com>
2023-11-18 11:31:52 +0000

    edit and move js to assets folder to reduce the rendering of resources

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+//Font Awesome kit
+src = "https://kit.fontawesome.com/ad080cc682.js"
+crossorigin = "anonymous";
+
+//Javascrip for the slide images https://www.w3schools.com/howto/howto_js_slideshow.asp
+document.addEventListener('DOMContentLoaded', function () {
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let slides = document.getElementsByClassName("myslides");
+        let dots = document.getElementsByClassName("dot");
+
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        for (let i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+
+    // listeners to prev and next buttons to prevent default link behavior when JS is enabled
+    document.querySelector('.prev').addEventListener('click', function (event) {
+        event.preventDefault();
+        plusSlides(-1);
+    });
+
+    document.querySelector('.next').addEventListener('click', function (event) {
+        event.preventDefault();
+        plusSlides(1);
+    });
+});
